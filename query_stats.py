d7a5bd0a4888c00d0914ff128bf78dc8f22c4d33
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2021-09-07 12:46:26 -0400

    Add throughput numbers in places

--- query_stats.py
+++ query_stats.py
@@ -7,6 +7,9 @@ import json
 import trino_api
 import utils
 
+def calculate_throughput(size, time):
+    return (size / time) if time > 0 else 0
+
 def parse_query_json(query_json):
     query_stats = query_json['queryStats']
     print('=== Query Info ===')
@@ -20,8 +23,11 @@ def parse_query_json(query_json):
     print('blocked splits  : ', query_stats['blockedDrivers'])
     output_stage_stats = query_json['outputStage']['stageStats']
     print('  === output stage stats ===')
-    print('  buffered   : ', output_stage_stats['bufferedDataSize'])
-    print('  output size: ', output_stage_stats['outputDataSize'])
+    print('  buffered                       : ', output_stage_stats['bufferedDataSize'])
+    print('  output size                    : ', output_stage_stats['outputDataSize'])
+    query_elapsed_time = utils.parse_read_time(query_json['queryStats']['elapsedTime'])
+    client_output_throughput = (utils.parse_read_data_size(output_stage_stats['outputDataSize']) / query_elapsed_time) if query_elapsed_time > 0 else 0
+    print('  client output throughput (MB/s): ', client_output_throughput)
     stages = utils.build_stages(query_json)
     print('  === Stage Stats ===')
     for stage in stages:
@@ -34,14 +40,19 @@ def parse_query_json(query_json):
         if stage_catalog_name is None:
             stage_catalog_name = "UNKNOWN"
         stage_stats = stage['stageStats']
-        print('  physical input data size : ', stage_stats['physicalInputDataSize'])
-        print('  physical input read time : ', stage_stats['physicalInputReadTime'])
-        print('  physical output data size: ', stage_stats['physicalWrittenDataSize'])
-        print('  total splits             : ', stage_stats['totalDrivers'])
-        print('  completed splits         : ', stage_stats['completedDrivers'])
-        print('  running splits           : ', stage_stats['runningDrivers'])
-        print('  queued splits            : ', stage_stats['queuedDrivers'])
-        print('  blocked splits           : ', stage_stats['blockedDrivers'])
+        physical_input_stage_throughput = calculate_throughput(
+            utils.parse_read_data_size(stage_stats['physicalInputDataSize']),
+            utils.parse_read_time(stage_stats['physicalInputReadTime'])
+        )
+        print('  physical input data size        : ', stage_stats['physicalInputDataSize'])
+        print('  physical input read time        : ', stage_stats['physicalInputReadTime'])
+        print('  physical input throughput (MB/s): ', physical_input_stage_throughput)
+        print('  physical output data size       : ', stage_stats['physicalWrittenDataSize'])
+        print('  total splits                    : ', stage_stats['totalDrivers'])
+        print('  completed splits                : ', stage_stats['completedDrivers'])
+        print('  running splits                  : ', stage_stats['runningDrivers'])
+        print('  queued splits                   : ', stage_stats['queuedDrivers'])
+        print('  blocked splits                  : ', stage_stats['blockedDrivers'])
         tasks = stage['tasks']
         print('    === Task Stats ===')
         for task in tasks:
@@ -49,14 +60,19 @@ def parse_query_json(query_json):
             worker_host = task['taskStatus']['self'].split("//")[1].split(":")[0]
             print('    worker is : ', worker_host)
             task_stats = task['stats']
-            print('    physical input data size : ', task_stats['physicalInputDataSize'])
-            print('    physical input read time : ', task_stats['physicalInputReadTime'])
-            print('    physical output data size: ', task_stats['physicalWrittenDataSize'])
-            print('    total splits             : ', task_stats['totalDrivers'])
-            print('    completed splits         : ', task_stats['completedDrivers'])
-            print('    running splits           : ', task_stats['runningDrivers'])
-            print('    queued splits            : ', task_stats['queuedDrivers'])
-            print('    blocked splits           : ', task_stats['blockedDrivers'])
+            physical_input_task_throughput = calculate_throughput(
+                utils.parse_read_data_size(task_stats['physicalInputDataSize']),
+                utils.parse_read_time(task_stats['physicalInputReadTime'])
+            )
+            print('    physical input data size        : ', task_stats['physicalInputDataSize'])
+            print('    physical input read time        : ', task_stats['physicalInputReadTime'])
+            print('    physical input throughput (MB/s): ', physical_input_task_throughput)
+            print('    physical output data size       : ', task_stats['physicalWrittenDataSize'])
+            print('    total splits                    : ', task_stats['totalDrivers'])
+            print('    completed splits                : ', task_stats['completedDrivers'])
+            print('    running splits                  : ', task_stats['runningDrivers'])
+            print('    queued splits                   : ', task_stats['queuedDrivers'])
+            print('    blocked splits                  : ', task_stats['blockedDrivers'])
 
 parser = argparse.ArgumentParser(description='Accepted parameters')
 parser.add_argument('-q', '--query', help='query ID', required=True)
--- slow_clients.py
+++ slow_clients.py
@@ -14,13 +14,16 @@ coordinator_uri = trino_config['http_scheme'] + '://' + trino_config['host'] + '
 verify_certs = trino_config['verify_certs'].lower() == 'true'
 
 def check_output_buffers(query_json):
+    query_elapsed_time = utils.parse_read_time(query_json['queryStats']['elapsedTime'])
     output_stage_stats = query_json['outputStage']['stageStats']
     buffered_size = utils.parse_read_data_size(output_stage_stats['bufferedDataSize'])
+    client_output_throughput = (utils.parse_read_data_size(output_stage_stats['outputDataSize']) / query_elapsed_time) if query_elapsed_time > 0 else 0
     if (buffered_size >= 32):
         print('(WARNING) root stage output buffer full for query: ', query_json['queryId'])
     print('=== output stats ===')
-    print('  buffered   : ', output_stage_stats['bufferedDataSize'])
-    print('  output size: ', output_stage_stats['outputDataSize'])
+    print('  buffered                       : ', output_stage_stats['bufferedDataSize'])
+    print('  output size                    : ', output_stage_stats['outputDataSize'])
+    print('  client output throughput (MB/s): ', client_output_throughput)
 
 for query in trino_api.current_queries(session, verify_certs, coordinator_uri, trino_config['user']):
     query_json = trino_api.get_query_json(session, verify_certs, coordinator_uri, trino_config['user'], query['queryId'])
